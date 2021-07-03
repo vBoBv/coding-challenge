977fe733b4fd96bdf1ba7ef701135f58c722701e
vBoBv <vannponhvath@gmail.com>
2021-07-03 15:16:20 +1200

    setup account interface

---
+++ project-9spokes/src/state/interfaces/account.ts
@@ -0,0 +1,13 @@
+export interface Account {
+	account_category: string;
+	account_code: string;
+	account_currency: string;
+	account_identifier: string;
+	account_status: string;
+	value_type: string;
+	account_name: string;
+	account_type: string;
+	account_type_bank: string;
+	system_account: string;
+	total_value: number;
+}
