28861f96d0b40775f342f13dc1667944f48db894
vBoBv <vannponhvath@gmail.com>
2021-07-03 16:05:58 +1200

    setup getAccountsData action creator

---
+++ project-9spokes/src/state/action-creators/account.ts
@@ -0,0 +1,24 @@
+import { Dispatch } from 'redux';
+import { AccountActionType } from '../action-types';
+import { AccountAction } from '../actions';
+import { Ledgers } from '../../api/generalLedgers';
+
+export const getAccountsData = () => {
+	return async (dispatch: Dispatch<AccountAction>) => {
+		dispatch({ type: AccountActionType.GET_ACCOUNT });
+
+		try {
+			const accounts = await Ledgers.getGeneralLedger();
+
+			dispatch({
+				type: AccountActionType.GET_ACCOUNT_SUCCESS,
+				payload: accounts.data
+			});
+		} catch (err) {
+			dispatch({
+				type: AccountActionType.GET_ACCOUNT_SUCCESS,
+				payload: err.message
+			});
+		}
+	};
+};
