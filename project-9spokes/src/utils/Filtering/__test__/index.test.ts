31604cdff383a325cee3a27d78968adbdcd26a0c
vBoBv <vannponhvath@gmail.com>
2021-07-05 00:21:46 +1200

    added tests for filtering functions

---
+++ project-9spokes/src/utils/Filtering/__test__/index.test.ts
@@ -0,0 +1,77 @@
+import { filterAccountCategory } from '../filterAccountCategory';
+import { filterAccountType } from '../filterAccountType';
+import { filterValueType } from '../filterValueType';
+import { mockData } from '../../../data/mockData';
+
+it('filters the account by account category', () => {
+	const output = [
+		{
+			account_category: 'revenue',
+			account_code: '200',
+			account_currency: 'AUD',
+			account_identifier: 'e2bacdc6-2006-43c2-a5da-3c0e5f43b452',
+			account_status: 'ACTIVE',
+			value_type: 'credit',
+			account_name: 'Sales',
+			account_type: 'sales',
+			account_type_bank: '',
+			system_account: '',
+			total_value: 32431.0
+		}
+	];
+
+	expect(filterAccountCategory(mockData, 'revenue')).toEqual(output);
+});
+
+it('filters the account by value type', () => {
+	const output = [
+		{
+			account_type_bank: '',
+			value_type: 'debit',
+			total_value: 310.0,
+			account_name: 'Cleaning',
+			account_type: 'overheads',
+			account_currency: 'AUD',
+			account_identifier: 'ff09eac3-5b17-44fb-9eea-e2e9375e91b4',
+			account_status: 'ACTIVE',
+			system_account: '',
+			account_category: 'expense',
+			account_code: '408'
+		},
+		{
+			account_category: 'assets',
+			account_name: 'Office Equipment',
+			total_value: 750.0,
+			account_status: 'ACTIVE',
+			account_type: 'fixed',
+			account_type_bank: '',
+			system_account: '',
+			value_type: 'debit',
+			account_code: '710',
+			account_currency: 'AUD',
+			account_identifier: '2d69b4ee-37d4-4f67-b950-32cbdd5765ed'
+		}
+	];
+
+	expect(filterValueType(mockData, 'debit')).toEqual(output);
+});
+
+it('filters the account by account type', () => {
+	const output = [
+		{
+			account_category: 'revenue',
+			account_code: '200',
+			account_currency: 'AUD',
+			account_identifier: 'e2bacdc6-2006-43c2-a5da-3c0e5f43b452',
+			account_status: 'ACTIVE',
+			value_type: 'credit',
+			account_name: 'Sales',
+			account_type: 'sales',
+			account_type_bank: '',
+			system_account: '',
+			total_value: 32431.0
+		}
+	];
+
+	expect(filterAccountType(mockData, ['sales'])).toEqual(output);
+});
