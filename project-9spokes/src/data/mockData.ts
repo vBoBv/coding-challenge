74efc1a23fbbcb127961813db91c624ab6df0700
vBoBv <vannponhvath@gmail.com>
2021-07-05 01:21:09 +1200

    added data for testings

--- project-9spokes/src/data/mockData.ts
+++ project-9spokes/src/data/mockData.ts
@@ -54,3 +54,87 @@ export const mockData: Account[] = [
 		system_account: 'HISTORICAL'
 	}
 ];
+
+export const sampleData = [
+	{
+		account_category: 'revenue',
+		account_code: '200',
+		account_currency: 'AUD',
+		account_identifier: 'e2bacdc6-2006-43c2-a5da-3c0e5f43b452',
+		account_status: 'ACTIVE',
+		value_type: 'debit',
+		account_name: 'Sales',
+		account_type: 'sales',
+		account_type_bank: '',
+		system_account: '',
+		total_value: 10
+	},
+	{
+		account_type_bank: '',
+		value_type: 'debit',
+		total_value: 30,
+		account_name: 'Cleaning',
+		account_type: 'overheads',
+		account_currency: 'AUD',
+		account_identifier: 'ff09eac3-5b17-44fb-9eea-e2e9375e91b4',
+		account_status: 'ACTIVE',
+		system_account: '',
+		account_category: 'expense',
+		account_code: '408'
+	}
+];
+
+export const capitalSampleData = [
+	{
+		account_category: 'assets',
+		account_code: '200',
+		account_currency: 'AUD',
+		account_identifier: 'e2bacdc6-2006-43c2-a5da-3c0e5f43b452',
+		account_status: 'ACTIVE',
+		value_type: 'debit',
+		account_name: 'Sales',
+		account_type: 'current',
+		account_type_bank: '',
+		system_account: '',
+		total_value: 100
+	},
+	{
+		account_type_bank: '',
+		value_type: 'credit',
+		total_value: 30,
+		account_name: 'Cleaning',
+		account_type: 'bank',
+		account_currency: 'AUD',
+		account_identifier: 'ff09eac3-5b17-44fb-9eea-e2e9375e91b4',
+		account_status: 'ACTIVE',
+		system_account: '',
+		account_category: 'assets',
+		account_code: '408'
+	},
+	{
+		account_category: 'liability',
+		account_code: '200',
+		account_currency: 'AUD',
+		account_identifier: 'e2bacdc6-2006-43c2-a5da-3c0e5f43b452',
+		account_status: 'ACTIVE',
+		value_type: 'credit',
+		account_name: 'Sales',
+		account_type: 'current',
+		account_type_bank: '',
+		system_account: '',
+		total_value: 40
+	},
+	{
+		account_type_bank: '',
+		value_type: 'debit',
+		total_value: 20,
+		account_name: 'Cleaning',
+		account_type: 'current_accounts_payable',
+		account_currency: 'AUD',
+		account_identifier: 'ff09eac3-5b17-44fb-9eea-e2e9375e91b4',
+		account_status: 'ACTIVE',
+		system_account: '',
+		account_category: 'liability',
+		account_code: '408'
+	}
+];
