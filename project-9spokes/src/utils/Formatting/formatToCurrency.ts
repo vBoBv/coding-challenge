14508a0ac8f936d5909fd2369ddd9b7ef3522a27
vBoBv <vannponhvath@gmail.com>
2021-07-05 00:44:53 +1200

    fixed maximumFractionDigits out of range bug

--- project-9spokes/src/utils/Formatting/formatToCurrency.ts
+++ project-9spokes/src/utils/Formatting/formatToCurrency.ts
@@ -2,7 +2,8 @@ export const formatToCurrency = (value: number, decimalDigit: number): string =>
 	const formatter = new Intl.NumberFormat('en-AU', {
 		style: 'currency',
 		currency: 'AUD',
-		maximumFractionDigits: decimalDigit
+		maximumFractionDigits: decimalDigit,
+		minimumFractionDigits: decimalDigit
 	});
 
 	const result = formatter.format(value);
